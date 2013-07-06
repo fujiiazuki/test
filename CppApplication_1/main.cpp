813ba3774ea7ba9fdea8511ea3987c4d144996f1
azuki <f.masaru@gol.com>
2013-07-06 17:19:52 +0900

    test

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: fujiimasaru
+ *
+ * Created on 2013/05/19, 8:15
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
